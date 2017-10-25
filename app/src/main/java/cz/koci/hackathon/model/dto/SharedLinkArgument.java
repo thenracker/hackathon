5bac62e73bdb54d7ce297c7b3d7a9334b65cbf40
Daniel Pichl <pichl@mobilesoft.eu>
2017-10-25 14:07:43 +0200

    Rest volání pro stažení metadat shared linku

--- app/src/main/java/cz/koci/hackathon/model/dto/SharedLinkArgument.java
+++ app/src/main/java/cz/koci/hackathon/model/dto/SharedLinkArgument.java
@@ -1,16 +1,26 @@
 package cz.koci.hackathon.model.dto;
 
+import android.support.annotation.Nullable;
+
 /**
  * Created by daniel on 25.10.17.
  */
 
 public class SharedLinkArgument {
 
+    private final String url;
     private final String path;
     private final SharedLinkSettings settings;
 
     public SharedLinkArgument(String path, SharedLinkSettings settings) {
         this.path = path;
         this.settings = settings;
+        this.url = null;
+    }
+
+    public SharedLinkArgument(String url, @Nullable String path) {
+        this.url = url;
+        this.path = path;
+        this.settings = null;
     }
 }
