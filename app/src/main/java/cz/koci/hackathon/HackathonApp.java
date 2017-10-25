40454e4f2c31bdeb57da5bcb044a23d2b3809a0e
Vladimir Tichy <vladimir.tichy@mobilesoft.cz>
2017-10-25 11:45:43 +0200

    Funkční autorizace a návrat zpět do fragmentu

--- app/src/main/java/cz/koci/hackathon/HackathonApp.java
+++ app/src/main/java/cz/koci/hackathon/HackathonApp.java
@@ -1,23 +1,23 @@
 package cz.koci.hackathon;
 
-import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by petrw on 25.10.2017.
  */
 
-public class HackathonApp extends Application {
+public class HackathonApp extends MultiDexApplication {
 
-    private static Context mContext;
+    private static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mContext = getContext();
+        context = this;
     }
 
     public static Context getContext() {
-        return mContext;
+        return context;
     }
 }
