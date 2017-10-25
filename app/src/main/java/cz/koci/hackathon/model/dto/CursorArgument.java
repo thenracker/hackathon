a6e6cce44f0dd7fb599f581e00bdd2fc46ff7cfd
Daniel Pichl <pichl@mobilesoft.eu>
2017-10-25 12:11:57 +0200

    Rest volání pro listování ve složce

---
+++ app/src/main/java/cz/koci/hackathon/model/dto/CursorArgument.java
@@ -0,0 +1,14 @@
+package cz.koci.hackathon.model.dto;
+
+/**
+ * Created by daniel on 25.10.17.
+ */
+
+public class CursorArgument {
+
+    private final String cursor;
+
+    public CursorArgument(String cursor) {
+        this.cursor = cursor;
+    }
+}
