e5ab88976f56c0b75872bf7e14f3542c6a784374
Petr Weissar <weissar.petr@gmail.com>
2017-10-25 10:01:30 +0200

    Develop: Funkční - jen network on main thread exception

--- app/src/main/java/cz/koci/hackathon/activities/MainActivity.java
+++ app/src/main/java/cz/koci/hackathon/activities/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
-            initDropBox();
+            //initDropBox();
         } catch (DbxException e) {
             //
         }
