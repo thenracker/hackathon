faea10c95fa3ba79a6f3728ec992745e2312170a
Vladimir Tichy <vladimir.tichy@mobilesoft.cz>
2017-10-25 15:21:08 +0200

    Fingerprint login

---
+++ app/src/main/java/cz/koci/hackathon/utils/FingerprintHelper.java
@@ -0,0 +1,85 @@
+package cz.koci.hackathon.utils;
+
+/**
+ * Created by vlado on 25/10/2017.
+ */
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+@TargetApi(Build.VERSION_CODES.M)
+public class FingerprintHelper extends FingerprintManager.AuthenticationCallback {
+    private final FingerprintCallback callback;
+
+    // You should use the CancellationSignal method whenever your app can no longer process user input, for example when your app goes
+    // into the background. If you don’t use this method, then other apps will be unable to access the touch sensor, including the lockscreen!//
+
+    private CancellationSignal cancellationSignal;
+    private Context context;
+
+    public FingerprintHelper(Context mContext, FingerprintCallback callback) {
+        context = mContext;
+        this.callback = callback;
+    }
+
+    //Implement the startAuth method, which is responsible for starting the fingerprint authentication process//
+
+    public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
+
+        cancellationSignal = new CancellationSignal();
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+    @Override
+    //onAuthenticationError is called when a fatal error has occurred. It provides the error code and error message as its parameters//
+
+    public void onAuthenticationError(int errMsgId, CharSequence errString) {
+        //I’m going to display the results of fingerprint authentication as a series of toasts.
+        //Here, I’m creating the message that’ll be displayed if an error occurs//
+
+        Toast.makeText(context, "Authentication error\n" + errString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+
+    //onAuthenticationFailed is called when the fingerprint doesn’t match with any of the fingerprints registered on the device//
+
+    public void onAuthenticationFailed() {
+        Toast.makeText(context, "Otisk neodpovídá", Toast.LENGTH_LONG).show();
+        callback.authenticationFailed();
+    }
+
+    @Override
+
+    //onAuthenticationHelp is called when a non-fatal error has occurred. This method provides additional information about the error,
+    //so to provide the user with as much feedback as possible I’m incorporating this information into my toast//
+    public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+        Toast.makeText(context, "Authentication help\n" + helpString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+
+    //onAuthenticationSucceeded is called when a fingerprint has been successfully matched to one of the fingerprints stored on the user’s device//
+    public void onAuthenticationSucceeded(
+            FingerprintManager.AuthenticationResult result) {
+
+        Toast.makeText(context, "Otisk v pořádku", Toast.LENGTH_LONG).show();
+        callback.authenticationSuccessfull();
+    }
+
+    public interface FingerprintCallback {
+        void authenticationFailed();
+        void authenticationSuccessfull();
+    }
+
+}
