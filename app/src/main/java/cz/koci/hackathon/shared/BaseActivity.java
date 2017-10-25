dd0199971b9e6012f69fb51b8a29416035bb3047
Petr Weissar <weissar.petr@gmail.com>
2017-10-25 10:23:43 +0200

    Develop: Fix pádu - špatný onCreate

--- app/src/main/java/cz/koci/hackathon/shared/BaseActivity.java
+++ app/src/main/java/cz/koci/hackathon/shared/BaseActivity.java
@@ -15,8 +15,8 @@ import butterknife.ButterKnife;
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(getLayoutResId());
         ButterKnife.bind(this);
     }
